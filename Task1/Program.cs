1fd70f09bb15bd7218e57ef092fc5f6e4d9e8090
IGOR <sahmen@mail.ru>
2022-12-06 20:07:04 +0300

    DZ 1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+{
+int result = 1;
+for (int i = 1; i <= b; i++)
+
+    {
+        result = result * a;
+    }
+    Console.WriteLine($"Число {a} в степени {b}: {result}");
+}
