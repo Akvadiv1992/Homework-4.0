c06479557dc5ab10177143864ae9dad19599cb7d
IGOR <sahmen@mail.ru>
2022-12-07 20:14:12 +0300

    DZ 2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0, d;
+
+while (a != 0)
+{    
+    d = a % 10;
+    a = a / 10;
+    sum = sum + d;
+}
+
+Console.WriteLine($"Сумма цифр ровна: {sum}");
